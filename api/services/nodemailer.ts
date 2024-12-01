bf5e18e9fd4530cc5a7975bb1b07aa50eb621569
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-12-01 12:33:21 -0300

    initial commit

--- api/services/nodemailer.ts
+++ api/services/nodemailer.ts
@@ -11,21 +11,41 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// Função para verificar a conexão SMTP
+const verifyTransporter = async () => {
+  await new Promise<void>((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.error('Erro na verificação do transporte:', error);
+        reject(error);
+      } else {
+        console.log('Servidor SMTP está pronto para enviar mensagens');
+        resolve();
+      }
+    });
+  });
+};
+
 const sendEmail = async (
   to: string,
   subject: string,
   body: any
 ): Promise<SentMessageInfo> => {
   try {
+    // Verifica a conexão antes de enviar o e-mail
+    await verifyTransporter();
+
     const info = await transporter.sendMail({
       from: process.env.MAIL_FROM || '',
       to,
       subject,
       html: body,
     });
+
+    console.log('E-mail enviado:', info);
     return info;
   } catch (error) {
-    console.error('Erro ao enviar email:', error);
+    console.error('Erro ao enviar e-mail:', error);
     throw error;
   }
 };
